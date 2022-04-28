f689e16867decde8f1ac6013f53d7ffce25e2c5d
thisbit <ek@stroj.mjesto>
2022-04-28 14:38:15 +0200

    clean

--- blocks/slider/slider.php
+++ blocks/slider/slider.php
@@ -2,10 +2,6 @@
 /**
  * Slider Block Template.
  *
- * @param   array $block The block settings and attributes.
- * @param   string $content The block inner HTML (empty).
- * @param   bool $is_preview True during AJAX preview.
- * @param   (int|string) $post_id The post ID this block is saved to.
  */
 
 require_once( APURI_BLOCKS_PATH . 'inc/no-direct.php' ); 
