2e120490a795c1cf73f0d8c7533f2afc0f980e75
thisbit <elvis.krstulovic@fokusgrupa.net>
2024-02-14 19:23:53 +0100

    loop over any slider block on page

--- assets/slider.js
+++ assets/slider.js
@@ -1,9 +1,14 @@
-var flky = new Flickity( '.slider', {
-	setGallerySize: false,
-	wrapAround: true,
-	resize: true,
-	freeScroll: true,
-	lazyLoad: 1,
-	arrowShape:  
-		'M 30.3054 58.7195L0.573275 30.0126L30.2929 0.292968L31.7071 1.70718L4.41422 29.0001L87 29.0001L87 31.0001L4.47541 31.0001L31.6946 57.2807L30.3054 58.7195 Z',
-});
+if ( document.querySelector('.slider') ) {
+	let sliders = document.querySelectorAll('.slider');
+	for (let i = 0; i < sliders.length; i++) {
+		let slider = new Flickity( sliders[i], {
+			setGallerySize: false,
+			wrapAround: true,
+			resize: true,
+			freeScroll: true,
+			lazyLoad: 1,
+			arrowShape:  
+				'M 30.3054 58.7195L0.573275 30.0126L30.2929 0.292968L31.7071 1.70718L4.41422 29.0001L87 29.0001L87 31.0001L4.47541 31.0001L31.6946 57.2807L30.3054 58.7195 Z',
+  	});
+	}
+}
